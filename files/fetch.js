e45804c5d52fd6610dedd180e72eeb3da493e459
Jakub Sancho Gożuk <70905916+JSanchoG@users.noreply.github.com>
2021-06-16 17:10:33 +0200

    Update fetch.js

--- files/fetch.js
+++ files/fetch.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
       fetchJSON += '<div class="row">';
       fetchJSON += '<div class="col-lg-8 col-md-10 mx-auto">';
       fetchJSON += '<div class="post-preview">';
-      fetchJSON += '<a href="https://linux.2tihub.xyz/' + value.referral + '">';
+      fetchJSON += '<a href="https://linux.tihub.pl/' + value.referral + '">';
       fetchJSON += '<h2 class="post-title"><i class="' + value.icon + '"></i> ' + value.subject + '</h2>';
       fetchJSON += '<h3 class="post-subtitle">' + value.description + '</h3>';
       fetchJSON += '</a>';
